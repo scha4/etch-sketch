e081a216e75a9011bace672b04e69ee0f1193e21
“Sam <scha4@gmu.edu>
2023-03-06 00:06:38 +0900

    added buttons

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let color = "black";
+
 document.addEventListener("DOMContentLoaded", function () {
   createBoard(32);
 });
@@ -11,7 +13,24 @@ function createBoard(size) {
 
   for (let i = 0; i < numDivs; i++) {
     let div = document.createElement("div");
-    div.style.backgroundColor = "red";
+    div.addEventListener("mouseover", colorDiv);
     board.insertAdjacentElement("beforeend", div);
   }
+}
+//color
+function colorDiv() {
+  if (color == "random") {
+    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+  } else {
+    this.style.backgroundColor = "black";
+  }
+}
+function setColor(colorChoice) {
+  color = colorChoice;
+}
+
+//Reset button
+function reset() {
+  let divs = document.querySelectorAll("div");
+  divs.forEach((div) => (div.style.backgroundColor = "white"));
 }
