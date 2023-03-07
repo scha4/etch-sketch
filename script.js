b0c6a43d22a67652ea692d109b9f52ebdf28a122
“Sam <scha4@gmu.edu>
2023-03-08 01:57:54 +0900

    final changes

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 let color = "black";
+const INITIAL_GRID = 32;
+let actual_grid = INITIAL_GRID;
 
+let click = false;
+let gridRange = document.getElementById("myRange");
 document.addEventListener("DOMContentLoaded", function () {
   createBoard(32);
+  document.querySelector("body").addEventListener("click", function (e) {
+    if (e.target.tagName != "BUTTON") {
+      click = !click;
+    }
+  });
 });
 
 function createBoard(size) {
@@ -19,8 +28,13 @@ function createBoard(size) {
 }
 //color
 function colorDiv() {
-  if (color == "random") {
-    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+  if (click) {
+    if (color == "random") {
+      this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    }
+    if (color == "erase") {
+      this.style.backgroundColor = "white";
+    }
   } else {
     this.style.backgroundColor = "black";
   }
@@ -34,3 +48,18 @@ function reset() {
   let divs = document.querySelectorAll("div");
   divs.forEach((div) => (div.style.backgroundColor = "white"));
 }
+
+// displaying slider
+let range = document.querySelector(".rangeValue");
+range.innerHTML = INITIAL_GRID + "  x  " + INITIAL_GRID;
+
+//change slider
+
+function changeValue() {
+  range.innerHTML = gridRange.value + "  x  " + gridRange.value;
+  actual_grid = gridRange.value;
+  createBoard(actual_grid);
+}
+
+gridRange.addEventListener("input", changeValue);
+gridRange.addEventListener("input", createBoard);
